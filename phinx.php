e4f2b289e5825284e37ac3b53d3fff621cccc3a7
ryinner <vinograd18062002@gmail.com>
2022-01-22 00:25:29 +0500

    Phinx try

---
+++ phinx.php
@@ -0,0 +1,46 @@
+<?php
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::create(__DIR__);
+$dotenv->load();
+
+return
+[
+    'paths' => [
+        'App\\Migrations' => '%%PHINX_CONFIG_DIR%%/src/DataBase/Migrations',
+        'App\\Seeds' => '%%PHINX_CONFIG_DIR%%/src/DataBase/Seeds'
+    ],
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_database' => 'development',
+        'production' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'production_db',
+            'user' => 'root',
+            'pass' => '',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ],
+        'development' => [
+            'adapter' => getenv('DB_ADAPTER'),
+            'host' => getenv('DB_HOST'),
+            'name' => getenv('DB_NAME'),
+            'user' => getenv('DB_USERNAME'),
+            'pass' => getenv('DB_PASSWORD'),
+            'port' => getenv('DB_PORT'),
+            'charset' => 'utf8',
+        ],
+        'testing' => [
+            'adapter' => 'mysql',
+            'host' => 'localhost',
+            'name' => 'testing_db',
+            'user' => 'root',
+            'pass' => '',
+            'port' => '3306',
+            'charset' => 'utf8',
+        ]
+    ],
+    'version_order' => 'creation'
+];
