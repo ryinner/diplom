fcc02b7944fbc41c3ed199cadabfa8ff9858a0e3
ryinner <vinograd18062002@gmail.com>
2022-06-13 21:43:02 +0500

    fix

--- src/Plugins/Mailer.php
+++ src/Plugins/Mailer.php
@@ -13,7 +13,7 @@ class Mailer
     public function __construct($config)
     {
         $this->mail = new PHPMailer(true);
-        $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+        // $this->mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
         $this->mail->isSMTP();                                            //Send using SMTP
         $this->mail->Host      = $config->host;                        //Set the SMTP server to send through
         $this->mail->SMTPAuth   = true;                                   //Enable SMTP authentication
@@ -41,10 +41,10 @@ class Mailer
         $this->mail->Subject =  $subject;
         $this->mail->Body    =  $message;
         $this->mail->AltBody = 'Спасибо, что работате с нами';
-        try {
+        // try {
             $this->mail->send();
-        } catch (Exception $e) {
-            var_dump($this->mail->ErrorInfo);
-        }
+        // } catch (Exception $e) {
+        //     var_dump($this->mail->ErrorInfo);
+        // }
     }
 }
